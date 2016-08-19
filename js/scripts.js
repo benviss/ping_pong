21412f1966544282bf67c361b8257ed99723f70d
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-08-19 10:45:09 -0700

    Add background image. Create functions to assign numbers to an array and add ping-pong to specific indexes. return information to front-end logic. Output answer to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 // Back End Logic
-var numberArray = [];
-function createNumbers(number) {
+
+function pingPong(number) {
+  var pingArray = [];
   for (var i = 1; i <= number; i++) {
-
+  if ((i % 15) === 0) {
+    pingArray.push('ping-pong!')
   }
+  else if ((i % 3) === 0) {
+    pingArray.push('ping!')
+  } else if((i % 5) === 0) {
+    pingArray.push('pong!')
+  } else
+    pingArray.push(i);
+  }
+  return pingArray;
 }
-
-
-
-
-
-
-
-
-
 
 // Front End Logic
 $(document).ready(function() {
-  $('form').submit(function() {
+
+  $('form').submit(function(event) {
+    $("ul").empty();
     var userInput = parseInt($('#userPing').val());
-
     if(userInput) {
-
+      var pingArray = pingPong(userInput)
+      for (var i = 0; i < pingArray.length; i++) {
+        $('<li>' + pingArray[i] + '</li>').appendTo('ul');
+      }
     } else {
       alert('Please only enter a number')
     }
+    event.preventDefault();
   });
 });
